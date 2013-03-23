642b03aca841131c8494c0a777fde8152f232073
Nick Freear <nfreear@yahoo.co.uk>
2013-03-23 16:58:30 +0000

    mustacherepeat.js : Mustache Template Repeater - initial commit
    * Based on: https://gist.github.com/nfreear/5223248
    * Copyright 2013 Nick Freear.
    * Javascript + HTML test skeleton.

---
+++ mustacherepeat.js
@@ -0,0 +1,79 @@
+/*
+ mustacherepeat.js : Mustache Template Repeater.
+ Copyright 2013 Nick Freear.
+*/
+
+/*jslint nomen: true, plusplus: true, todo: true, white: true, indent: 2 */
+/* global Mustache document */
+
+var MustacheRepeat = function (op) {
+  "use strict";
+
+  op = op || {};
+  op.compiler = op.compiler || Mustache;
+  op.template = op.template || '#template';
+  op.target = op.target || '#target';
+  op.addBtn = op.addBtn || '#addBtn';
+  op.start = parseInt(op.start) || 1;
+  op.min = parseInt(op.min) || 1;
+  op.max = parseInt(op.max) || 10;
+  op.onadd = op.onadd || function () {};
+  op.ondelete = op.ondelete || function () {};
+  op.predelete = op.predelete || function () { return true; };
+  op.deleteText = op.deleteText || "Remove";
+  op.data = op.data || {};
+
+  var idx = 0,
+    //self = this,
+    select = function (sel) {
+      return document.querySelector(sel);
+    },
+    temp_elem = select(op.template),
+    template = temp_elem ? temp_elem.innerHTML : null,
+    compiledTemp = op.compiler.compile(template),
+    target = select(op.target),
+    j,
+
+    // TODO - handle errors?
+
+    _delete = function (ev, i) {
+      var del = op.predelete(i, op, ev);
+      if (!del) {
+        console.log('No delete');
+        return del;
+      }
+
+      // TODO.
+
+      op.ondelete(i, op, ev);
+    },
+    add = function (ev) {
+      if (idx >= op.max) {
+        return;
+      }
+      target.innerHTML += compiledTemp({
+        idx: idx, data: op.data, deleteText: op.deleteText, _deleteCmd: "mr.delete("+ idx +")" });
+      // Bug in onclick assign?
+      (function (i) {
+        select('#mr-del-' + i).onclick = function (ev) { _delete(ev, i); };
+      })(idx);
+
+      op.onadd(idx, op, ev);
+
+      idx++;
+    };
+
+  // Initialize!
+  for (j = 0; j < op.start; j++) {
+    add();
+  }
+  select(op.addBtn).onclick = function (ev) { add(ev); };
+
+  return {
+    options: op,
+
+    // Public methods.
+    add: add,
+    _delete: _delete
+  };
+};
